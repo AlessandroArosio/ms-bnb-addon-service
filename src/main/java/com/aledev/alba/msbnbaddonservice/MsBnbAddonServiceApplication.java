42ec51493119d43b3ea51a965225f68cb28a62e6
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-05-03 14:34:35 +0100

    adding new base project

---
+++ src/main/java/com/aledev/alba/msbnbaddonservice/MsBnbAddonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbaddonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBnbAddonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBnbAddonServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aledev/alba/msbnbaddonservice/MsBnbAddonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aledev.alba.msbnbaddonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBnbAddonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
